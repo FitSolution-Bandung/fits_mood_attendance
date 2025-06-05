f5788b5d11f69411bad2fc538dd151aeda43261d
wahyubayuobay <bayu@fujicon.id>
2025-06-05 11:12:08 +0700

    Update Banner dan Manifest

--- fits_employees_mood/__manifest__.py
+++ fits_employees_mood/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "fits_employees_mood",
+    'name': "MoodPresence - Employee Attendance with Mood Tracking",
 
     'summary': """
         Addon Odoo yang mengintegrasikan absensi karyawan dengan pelacakan suasana hati (mood tracking) 
         untuk meningkatkan pemahaman terhadap kesejahteraan dan produktivitas tenaga kerja""",
 
     'description': """
-        Long description of module's purpose
+        Addon ini memungkinkan karyawan melaporkan suasana hati mereka setiap kali melakukan check-in atau check-out, sehingga perusahaan dapat memantau pola kesejahteraan karyawan dan mengambil langkah proaktif untuk meningkatkan produktivitas serta lingkungan kerja yang lebih sehat.
     """,
 
     'author': "PT Fujicon Priangan Perdana",
@@ -18,7 +18,10 @@
     # for the full list
     'category': 'Human Resource',
     'version': '0.1',
-
+    'images': ['images/banner.gif'],
+    'price': 21.20,
+    'currency': 'USD',
+    'license': 'AGPL-3',
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr_attendance','hr'],
 
